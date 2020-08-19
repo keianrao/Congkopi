01dc0c9d6e308a46f29e7337fa1498ceed742370
Keian Rao <48932730+keianrao@users.noreply.github.com>
2020-08-19 08:53:37 -0400

    Temporarily adding program 'SeedDemo'
    'SeedDemo' is a proof-of-concept for how seed rendering will work

---
+++ SeedDemo.java
@@ -0,0 +1,115 @@
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
+import java.util.*;
+import java.util.List;
+import java.io.IOException;
+
+class SeedDemo extends JPanel {
+
+private static final Color FOREGROUND = Color.GRAY;
+private static final Color ACCENT = FOREGROUND.darker();
+private static final Color BACKGROUND = Color.BLACK;
+
+private final List<Seed> seeds = new ArrayList<Seed>(32);
+
+//  \\  //  \\  //  \\  //  \\  //  \\  //
+
+public SeedDemo() {
+	setBackground(BACKGROUND);
+	setForeground(FOREGROUND);
+}
+
+public static void main(String... args) throws IOException {
+	JFrame mainframe = new JFrame("Seed");
+	mainframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	SeedDemo instance = new SeedDemo();
+
+	mainframe.setContentPane(instance);
+	mainframe.setSize(256, 256);
+
+	mainframe.setVisible(true);
+
+	System.out.print("Hit return at any time to generate an extra seed: ");
+	System.out.flush();
+	while (System.in.read() != -1) {
+		System.in.skip(System.in.available());
+		instance.generateSeed();
+	}
+}
+
+//  \\  //  \\  //  \\  //  \\  //  \\  //
+
+public void generateSeed() {
+	Seed seed = new Seed(
+		128, 32,
+		Math.random() * (2 * Math.PI)
+	);
+
+	SwingUtilities.invokeLater(new Runnable() {
+		public void run() {
+			seeds.add(seed);
+			repaint();
+			Toolkit.getDefaultToolkit().sync();
+		}
+	});
+}
+
+//  \\  //  \\  //  \\  //  \\  //  \\  //
+
+public void paintComponent(Graphics g) {
+	if (!(g instanceof Graphics2D)) return;
+	Graphics2D g2d = (Graphics2D)g;
+
+	// Fill with background..
+	g2d.setColor(BACKGROUND);
+	g2d.fillRect(0, 0, getWidth(), getHeight());
+
+	// Now render seeds.
+	AffineTransform moveToCentre = 
+		AffineTransform.getTranslateInstance(
+			getWidth() / 2,
+			getHeight() / 2
+		);
+	for (Seed seed: seeds) {
+		Shape renderedShape = 
+			moveToCentre.createTransformedShape(seed.rotatedShape);
+		g2d.setColor(FOREGROUND);
+		g2d.fill(renderedShape);
+		g2d.setColor(ACCENT);
+		g2d.draw(renderedShape);
+	}
+}
+
+//  \\  //  \\  //  \\  //  \\  //  \\  //
+
+static class Seed {
+	// int centreX, centreY;
+	/*
+	* Firstly, these should be separate from unrotatedShape so that
+	* rotation won't mess up (classic problem, try it).
+	* Secondly, we won't use it for now, we'll always repaint seeds
+	* at the centre of the window, for this demo.
+	*/
+	Shape unrotatedShape;
+	Shape rotatedShape;
+
+	Seed(int length, int width, double rotationInRadians) {
+		unrotatedShape = new Ellipse2D.Double(
+			-width / 2, -length / 2, 
+			width, length
+		);
+		rotatedShape = 
+			AffineTransform.getRotateInstance(-rotationInRadians/*[1]*/)
+				.createTransformedShape(unrotatedShape);
+		/*
+		* [1] We assume rotation given is clockwise, but anticlockwise is
+		* assumed by AffineTransform#getRotateInstance(double).
+		* I think simply negating the value will swap directions like so.
+		*/
+	}
+}
+
+}
